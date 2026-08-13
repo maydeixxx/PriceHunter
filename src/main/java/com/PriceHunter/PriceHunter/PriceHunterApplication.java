174b7eceb278a5e3d75a890665bebe117dc361b3
Alex Karev <amaevalx@gmail.com>
2026-08-13 17:00:00 +0700

    Init commit

---
+++ src/main/java/com/PriceHunter/PriceHunter/PriceHunterApplication.java
@@ -0,0 +1,13 @@
+package com.PriceHunter.PriceHunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceHunterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PriceHunter/PriceHunter/PriceHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PriceHunter.PriceHunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
