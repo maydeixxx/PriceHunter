221ded56d29950950323b2155c0ba2b7423ae3e0
Alex Karev <amaevalx@gmail.com>
2026-09-07 12:09:08 +0700

    started building gatway api

--- GatewayAPI/src/main/java/com/PriceHunter/GatewayAPI/GatewayApiApplication.java
+++ GatewayAPI/src/main/java/com/PriceHunter/GatewayAPI/GatewayApiApplication.java
@@ -1,4 +1,4 @@
-package com.PriceHunter.AuthService;
+package com.PriceHunter.GatewayAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AuthServiceApplication {
+public class GatewayApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
+		SpringApplication.run(GatewayApiApplication.class, args);
 	}
 
 }
